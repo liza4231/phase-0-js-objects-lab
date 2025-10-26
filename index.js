2fcd74f880e8251af494cb80e4fb3faee6290543
Elizabeth Salazar <liza4231@gmail.com>
2025-10-25 20:20:36 -0500

    1st attempt

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 //Write your code here
+let attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+};
 
 
+function logAttendeeName(attendeeObj) {
+  console.log(attendeeObj.name);
+}
+
+
+function logTicketPrice(attendeeObj) {
+  console.log(attendeeObj.ticketPrice);
+}
+
+
+function updateTicketType(attendeeObj, newType) {
+  attendeeObj.ticketType = newType;
+}
+
+
+function updateTicketPrice(attendeeObj, newPrice) {
+  attendeeObj.ticketPrice = newPrice;
+}
+
+
+function removeEventProperty(attendeeObj) {
+  delete attendeeObj.event;
+}
+
+function addCheckedInProperty(attendeeObj) {
+  attendeeObj.checkedIn = true;
+}
 
 //Needed for the tests to work. Don't modify
 module.exports = {
